5bc6647ff50256d5b5a4ef0e7090eeeea610319b
Usman Musa <usmanmusa1920@gmail.com>
2022-11-07 21:03:27 +0100

    nginx staticfiles are served now! but still precautions have to be taken

--- dipense/dipense/settings.py
+++ dipense/dipense/settings.py
@@ -132,13 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
+STATIC_ROOT = '/static'
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
         ]
-else:
-    STATIC_ROOT = '/static/'
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static')
+#         ]
+# else:
+#     STATIC_ROOT = '/static/'
+#     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

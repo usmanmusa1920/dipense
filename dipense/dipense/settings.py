6d1b7adb9ab443ca153d1129b4fab2eed7561ad0
Usman Musa <usmanmusa1920@gmail.com>
2022-11-09 12:20:46 +0100

    change static_root in settings.py

--- dipense/dipense/settings.py
+++ dipense/dipense/settings.py
@@ -132,12 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-if DEBUG:
-  STATIC_ROOT = '/static'
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
